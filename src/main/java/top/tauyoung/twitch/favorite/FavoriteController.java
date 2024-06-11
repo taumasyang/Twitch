eb76b73596fc6096b24c11d6d7542d4a1286e587
tauyoung <tauyoung@icloud.com>
2024-06-11 23:57:10 +0800

    Auth Principal

--- src/main/java/top/tauyoung/twitch/favorite/FavoriteController.java
+++ src/main/java/top/tauyoung/twitch/favorite/FavoriteController.java
@@ -1,6 +1,8 @@
 package top.tauyoung.twitch.favorite;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,34 +13,38 @@ import org.springframework.web.server.ResponseStatusException;
 import top.tauyoung.twitch.db.entity.UserEntity;
 import top.tauyoung.twitch.model.FavoriteRequestBody;
 import top.tauyoung.twitch.model.TypeGroupedItemList;
+import top.tauyoung.twitch.user.UserService;
 
 @RestController
 @RequestMapping("/favorite")
 public class FavoriteController {
 	private final FavoriteService favoriteService;
-	// TODO: Replace the hard-coded temporary user
-	private final UserEntity userEntity = new UserEntity(1L, "user0", "Foo", "Bar", "password");
+	private final UserService userService;
 
-	public FavoriteController(FavoriteService favoriteService) {
+	public FavoriteController(FavoriteService favoriteService, UserService userService) {
 		this.favoriteService = favoriteService;
+		this.userService = userService;
 	}
 
 	@GetMapping
-	public TypeGroupedItemList getFavoriteItems() {
+	public TypeGroupedItemList getFavoriteItems(@AuthenticationPrincipal User user) {
+		UserEntity userEntity = userService.findByUsername(user.getUsername());
 		return favoriteService.getGroupedFavoriteItems(userEntity);
 	}
 
 	@PostMapping
-	public void setFavoriteItem(@RequestBody FavoriteRequestBody body) {
+	public void setFavoriteItem(@AuthenticationPrincipal User user, @RequestBody FavoriteRequestBody body) {
+		UserEntity userEntity = userService.findByUsername(user.getUsername());
 		try {
 			favoriteService.setFavoriteItem(userEntity, body.favorite());
 		} catch (DuplicateFavoriteException e) {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate entry for favorite record", e);
+			throw new ResponseStatusException(HttpStatus.CONFLICT, "Duplicate entry for favorite record", e);
 		}
 	}
 
 	@DeleteMapping
-	public void unsetFavoriteItem(@RequestBody FavoriteRequestBody body) {
+	public void unsetFavoriteItem(@AuthenticationPrincipal User user, @RequestBody FavoriteRequestBody body) {
+		UserEntity userEntity = userService.findByUsername(user.getUsername());
 		favoriteService.unsetFavoriteItem(userEntity, body.favorite().twitchId());
 	}
 }
